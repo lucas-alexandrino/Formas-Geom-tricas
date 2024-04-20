19f943f7c62e78530478148ef3bb83d571a97565
Lucas Alexandrino <lucas.alexcontato@gmail.com>
2024-04-20 17:23:06 -0300

    Cuboide e poligono

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,19 @@ function calcularAreaRetangulo() {
     const area = base * altura;
     document.getElementById('resultado').innerText = `A área do retângulo é ${area.toFixed(2)}`;
 }
+
+function calcularAreaCuboide() {
+    const comprimento = parseFloat(document.getElementById('comprimento').value);
+    const largura = parseFloat(document.getElementById('largura').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const area = 2 * (comprimento * largura + comprimento * altura + largura * altura);
+    document.getElementById('resultado').innerText = `A área do Cubóide é ${area.toFixed(2)}`;
+}
+
+function calcularAreaPoligonoRegular() {
+    const lados = parseFloat(document.getElementById('lados').value);
+    const lado = parseFloat(document.getElementById('lado').value);
+    const apotema = parseFloat(document.getElementById('apotema').value);
+    const area = (lados * lado * apotema) / 2;
+    document.getElementById('resultado').innerText = `A área do Polígono Regular é ${area.toFixed(2)}`;
+}
